195eb36d11117406aebc7234c57a7447a68375d3
mdcv22 <32253690+mdcv22@users.noreply.github.com>
2017-10-26 18:13:08 -0600

    contraseña_nueva

--- src/Controller/ForgotPassController.java
+++ src/Controller/ForgotPassController.java
@@ -5,18 +5,29 @@
  */
 package Controller;
 
+import Model.Conection;
 import java.net.URL;
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.SelectionModel;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javax.swing.JOptionPane;
 
 /**
  * FXML Controller class
@@ -28,16 +39,10 @@ public class ForgotPassController implements Initializable {
     Model.Conection conn = new Model.Conection();
     CallableStatement stmt;
     ResultSet rs;
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    Connection cn = conn.conexion();
     
     @FXML
-    private ComboBox cmb_Question;
+    public ComboBox<String> cmb_Question;
     @FXML
     private TextField txt_answer;
     @FXML
@@ -52,44 +57,86 @@ public class ForgotPassController implements Initializable {
     private Label lbl_confirmP;
     @FXML
     private Button btn_save;
+    @FXML
+    public ChoiceBox<String> cmb_preguntas;
+    @FXML
+    public Pane pane_2;
+     
+    
+//    ArrayList<String> items = new ArrayList<>();
+      ObservableList<String> items = FXCollections.observableArrayList();
+   
+//    @FXML
+//    public void initialize() {
+//        cmb_Question.getItems().removeAll(cmb_Question.getItems());
+//        cmb_Question.getItems().addAll("Option A", "Option B", "Option C");
+//        cmb_Question.getSelectionModel().select("Option B");
+//    }
+
+     @Override
+    public void initialize(URL url, ResourceBundle rb) {
+            Preguntas();
+//            cmb_Question.setItems(items);
+//            
+//          cmb_Question.getItems().removeAll(cmb_Question.getItems());
+//          cmb_Question.getItems().addAll("Option A", "Option B", "Option C");
+//          cmb_Question.getSelectionModel().select("Option B");
+
+            cmb_preguntas.setItems(items);
+    }
+    
+    public void Preguntas(){
+        try {
+            stmt = cn.prepareCall("{CALL listar_pre()}");
+            rs = stmt.executeQuery();
+            while (rs.next()){
+                items.add(rs.getString(1));
+            }
+        } catch (Exception e) {
+        }
+    }
     
     public void CheckSecQ(){
-        if (txt_answer.getText().isEmpty() && !txt_User.getText().isEmpty()){
-            //Metodo log in
-            Connection cn = conn.conexion();
+        if (txt_User.getText().length() != 0 && !txt_answer.getText().isEmpty()){
             try {
-                stmt = cn.prepareCall("{select `login`(?, ?, ?)}");
+                stmt = cn.prepareCall("{CALL Sec_Ques(?,?,?)}");
                 stmt.setInt(1, Integer.parseInt(txt_User.getText()));
-                stmt.setInt(2,cmb_Question.getSelectionModel().getSelectedIndex() + 1);     
+                stmt.setInt(2,cmb_preguntas.getSelectionModel().getSelectedIndex() + 1);     
                 stmt.setString(3, txt_answer.getText());
-                //ERROR
-                int result;
+                int result = 0;
                 rs = stmt.executeQuery();
                 while(rs.next()) {
-                    result =rs.getInt(1);
+                    result = rs.getInt(1);
                 }
-                
-                        
+                Enable(result);
             } catch (Exception e) {
             }
-            
         }
     }
     
     public void Enable(int result){
         if (result == 1){
-            lbl_newP.setDisable(false);
-            lbl_confirmP.setDisable(false);
-            txt_Pass1.setDisable(false);
-            txt_Pass2.setDisable(false);
-            btn_save.setDisable(false);
+//            txt_Pass1.setDisable(false);
+//            txt_Pass2.setDisable(false);
+//            btn_save.setDisable(false);
+//            lbl_newP.setDisable(false);
+//            lbl_confirmP.setDisable(false);
+              pane_2.setDisable(false);
         }
         else {
-            //Mensaje pregunta y respuesta no coincide
+           JOptionPane.showMessageDialog(null, "ERROR", "ERROR", JOptionPane.ERROR_MESSAGE);
         }
     }
     
     public void Save(){
-        
+        if(!txt_Pass1.getText().isEmpty() && !txt_Pass2.getText().isEmpty()){
+            try {
+                stmt = cn.prepareCall("{CALL newPass(?,?)}");
+                stmt.setString(1, txt_Pass1.getText());
+                stmt.setString(2, txt_Pass2.getText());   
+                stmt.execute();
+            } catch (Exception e) {
+            }
+        }
     }
 }
---
+++ src/Model/ForgotPassModel.java
@@ -0,0 +1,32 @@
+package Model;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import java.net.URL;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author bberc
+ */
+public class ForgotPassModel {
+    
+    
+//     public static void Preguntas(Conection con, ObservableList item){
+//         CallableStatement stmt;
+//         ResultSet rs;
+//         Connection cn = con.conexion();
+//         try {
+//            stmt = cn.prepareCall("{listar_pre()}");
+//            rs = stmt.executeQuery();
+//            while (rs.next()){
+//                item.add(rs.getString(1));
+//                cmb_Question.setItems(items);
+//            }
+//        } catch (Exception e) {
+//        }
+//    }
+}
